f6d474f42794f9d2d101e467beaa61061aff4828
Martin Schoeberl <martin@jopdesign.com>
2025-11-24 21:18:42 +0100

    [caravel] add tnt's register file

--- src/main/scala/caravel/RegisterFileTest.scala
+++ src/main/scala/caravel/RegisterFileTest.scala
@@ -28,7 +28,49 @@ class RegisterFileTest extends Module {
     accessPhase := 1.B
   }
 
-  io.wb.dat_o := 0xDEADBEEFL.U
+  val rf = Module(new RegFileBlackbox)
+
+  rf.io.clk := clock
+
+  rf.io.w_data := io.wb.dat_i
+  rf.io.w_addr := io.wb.adr
+  rf.io.w_ena := io.wb.we
+
+  rf.io.ra_addr := io.wb.adr
+  val outa = rf.io.ra_data
+  rf.io.rb_addr := io.wb.adr
+  val outb = rf.io.rb_data
+  io.wb.dat_o := outa
+  // read the other port
+  when (io.wb.adr(5)) {
+    io.wb.dat_o := outb
+  }
+
   io.wb.ack := accessPhase
 
+}
+/*
+   rf_top i_rf(
+      .w_data(w_data),
+      .w_addr(w_addr),
+      .w_ena(w_ena),
+      .ra_addr(ra_addr),
+      .rb_addr(rb_addr),
+      .ra_data(ra_data),
+      .rb_data(rb_data),
+      .clk(clk)
+   );
+ */
+class RegFileBlackbox() extends BlackBox {
+  override def desiredName: String = "rf_top"
+  val io = IO(new Bundle {
+    val clk = Input(Clock())
+    val w_data = Input(UInt(32.W))
+    val w_addr = Input(UInt(5.W))
+    val w_ena = Input(Bool())
+    val ra_addr = Input(UInt(5.W))
+    val ra_data = Output(UInt(32.W))
+    val rb_addr = Input(UInt(5.W))
+    val rb_data = Output(UInt(32.W))
+  })
 }
